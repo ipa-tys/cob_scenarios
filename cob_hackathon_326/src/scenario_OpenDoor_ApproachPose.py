870ea745a64bfdc36b6d80d9939c28804be48e05
ipa-tys <tys@ipa.fhg.de>
2012-06-28 16:00:51 +0200

    state test (open door+approach pose) completed

--- cob_hackathon_326/src/scenario_OpenDoor_ApproachPose.py
+++ cob_hackathon_326/src/scenario_OpenDoor_ApproachPose.py
@@ -6,18 +6,20 @@ import rospy
 import smach
 import smach_ros
 from ApproachPose import *
-from WaitForOpenElevatorDoors import *
-from simple_script_server import *  # import script
-sss = simple_script_server()
+# from WaitForOpenElevatorDoors import *
+from WaitForSingleElevatorDoor import *
+#from simple_script_server import *  # import script
+#sss = simple_script_server()
 
 class SM1(smach.StateMachine):
     def __init__(self):
         smach.StateMachine.__init__(self,outcomes=['ended','failed'])
         with self:
             #  [-0.378, -6.839, 1.476]
-            smach.StateMachine.add('APPROACH_POSE',ApproachPose([-0.2,-3.9,1.4]),transitions={'reached':'WAITFOROPENDOOR', 'not_reached':'ended', 'failed':'failed'})
-            smach.StateMachine.add('WAITFOROPENDOOR',WaitForOpenElevatorDoors(rospy.Duration(15)),transitions={'door_open':'APPROACH_ENDPOSE', 'door_closed':'ended', 'failed':'failed'})
-            smach.StateMachine.add('APPROACH_ENDPOSE',ApproachPose(),transitions={'reached':'ended', 'not_reached':'ended', 'failed':'failed'})
+            # smach.StateMachine.add('APPROACH_POSE',ApproachPose([-0.2,-3.9,1.4]),transitions={'reached':'WAITFOROPENDOOR', 'not_reached':'ended', 'failed':'failed'})
+            #smach.StateMachine.add('WAITFOROPENDOOR',WaitForSingleElevatorDoor(rospy.Duration(15)),transitions={'door_open':'APPROACH_ENDPOSE', 'door_closed':'ended', 'failed':'failed'})
+            smach.StateMachine.add('WAITFOROPENDOOR',WaitForSingleElevatorDoor(rospy.Duration(15)),transitions={'door_open':'ended', 'door_closed':'ended', 'failed':'failed'})
+            #smach.StateMachine.add('APPROACH_ENDPOSE',ApproachPose(),transitions={'reached':'ended', 'not_reached':'ended', 'failed':'failed'})
 
 def main():
     rospy.init_node('AUTOMATICA2012')
